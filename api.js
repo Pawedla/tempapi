0707614f6e8f42bcdc6b36c29b163bc73413fbad
Pawedla <pedamain@gmx.at>
2022-02-22 12:13:19 +0100

    changed port

--- api.js
+++ api.js
@@ -1,12 +1,9 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(process.env.PORT || 5000, () => console.log('Server running at port 5000'));
